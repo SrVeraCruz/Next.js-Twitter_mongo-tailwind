d4ab9ed7f9311abbebb96881a14c2e1ba5f6b64f
Dúdú Vera Cruz <130259216+SrVeraCruz@users.noreply.github.com>
2024-08-21 19:48:23 +0100

    feat: Who to follow feature and Fixing edit image profile

--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -3,8 +3,16 @@
 import Layout from '../../components/layout/Layout'
 import WhoToFollow from '../../components/whoToFollow/WhoToFollow'
 import React from 'react'
+import useUserInfo from '../../hooks/useUserInfo'
+import { redirect } from 'next/navigation'
 
 export default function ExplorePage() {
+  const {userInfo, status} = useUserInfo()
+
+  if(!userInfo?._id && status !== 'loading') {
+    redirect('/login')
+  }
+
   return (
     <Layout>
       <WhoToFollow explore />
